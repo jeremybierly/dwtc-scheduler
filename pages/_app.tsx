afff0488f52a9848e0e8a318746713cbdd02e707
Jeremy Bierly <jeremybierly@me.com>
2021-12-31 10:43:16 -0600

    updates login ui, adds localStorage hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,34 @@
 import "../styles/globals.css";
+import { useLocalStorage } from "../hooks/uselocalStorage";
 
 function MyApp({ Component, pageProps }) {
+  const [userName, setUserName] = useLocalStorage("userName", "");
+  function logOut() {
+    setUserName("");
+    window.location.href = "/";
+  }
   return (
     <div className="dark:bg-gray-800 p-5 min-h-screen">
-      <h1 className="text-3xl text-gray:700 dark:text-gray-300">
-        DWTC Schedule
-      </h1>
+      <div className="flex justify-between">
+        <h1 className="text-3xl text-gray:700 dark:text-gray-300">
+          DWTC Schedule
+        </h1>
+        {userName === "" ? (
+          <a
+            href="/login/"
+            className="p-2 bg-gray-300 text-gray-800 dark:bg-gray-700 dark:text-gray-200 rounded font-bold"
+          >
+            Log In
+          </a>
+        ) : (
+          <button
+            className="p-2 bg-gray-300 text-gray-800 dark:bg-gray-700 dark:text-gray-200 rounded font-bold"
+            onClick={logOut}
+          >
+            Log Out
+          </button>
+        )}
+      </div>
       <Component {...pageProps} />
     </div>
   );
