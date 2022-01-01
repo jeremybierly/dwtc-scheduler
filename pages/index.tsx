cd0a1b133d889b6c85a75356f18180faa774e681
jeremybierly <jeremybierly@hotmail.com>
2022-01-01 09:51:55 -0600

    redirects on homepage and schedule page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,173 +1,21 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import { FirebaseApp, getApps, initializeApp } from "firebase/app";
-import { set, ref, Database, getDatabase } from "firebase/database";
-import { useObject } from "react-firebase-hooks/database";
-import { TimeSlotDisplay } from "../components/timeslot";
-import { useLocalStorage } from "../hooks/uselocalStorage";
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-};
-
-let firebaseApp: FirebaseApp;
-
-if (getApps().length === 0) {
-  firebaseApp = initializeApp(firebaseConfig);
-}
-
-// Get a reference to the database service
-const database: Database = getDatabase(firebaseApp);
+import { useState, useEffect } from "react"
 
 const Home: NextPage = () => {
-  const [userName, setUserName] = useLocalStorage("userName", "");
-  let day = new Date();
-  let today =
-    day.getFullYear() + "-" + (day.getMonth() + 1) + "-" + day.getDate();
+  const [today, setToday] = useState("2022-01-01")
 
-  const path: string = "schedule/" + today;
 
-  const [snapshot] = useObject(ref(database, path));
-  const courtSlots = snapshot && snapshot.val();
-  console.log(courtSlots);
-
-  let slots: string[] = [
-    "08:00 - 10:00 AM",
-    "10:00 - 12:00 PM",
-    "12:00 - 02:00 PM",
-    "02:00 - 04:00 PM",
-    "04:00 - 06:00 PM",
-    "06:00 - 07:30 PM",
-    "07:30 - 09:00 PM",
-    "09:00 - 10:30 PM",
-  ];
-
-  let user = {
-    userName: "error",
-  };
-
-  user.userName = userName || "error";
-
-  function reserveSlot(
-    courtSlot: string,
-    court: string,
-    slot: string,
-    userName: string
-  ): void {
-    if (userName === "error")
-      return alert("you must be logged in to update the schedule");
-    if (
-      confirm(
-        `Are you sure you want to set a reservation for ${court} from ${slot}?`
-      )
-    ) {
-      set(ref(database, "schedule/" + today + "/" + courtSlot), userName);
-    }
-  }
-
-  function cancelSlot(courtSlot: string, court: string, slot: string): void {
-    if (confirm(`Are you sure you want to cancel for ${court} from ${slot}?`)) {
-      set(ref(database, "schedule/" + today + "/" + courtSlot), "");
-    }
-  }
-
-  function changeDate(event: any): void {
-    window.location.href = `/schedule/${event.target.value}`;
-  }
+  useEffect(()=> {
+    let day = new Date()
+    setToday(day.getFullYear() + "-" + (day.getMonth() + 1) + "-" + day.getDate())
+    window.location.href="/schedule/"+ today
+  },[])
+    
+  
 
   return (
     <>
-      <Head>
-        <title>DWTC Schedule</title>
-      </Head>
-      <input
-        className="my-4 w-[200px] text-2xl border-2 border-gray-500 text-gray-700 bg-gray-300 dark:bg-gray-700 dark:text-gray-300 rounded "
-        type="date"
-        value={today}
-        onChange={changeDate}
-      />
-      <ul className="lg:grid lg:grid-cols-4 lg:gap-4 lg:mt-4">
-        <li className="hidden lg:block dark:text-gray-300 text-sm uppercase font-bold tracking-wide">
-          Time
-        </li>
-        <li className="hidden lg:block dark:text-gray-300 text-sm uppercase font-bold tracking-wide">
-          Court 1
-        </li>
-        <li className="hidden lg:block dark:text-gray-300 text-sm uppercase font-bold tracking-wide">
-          Court 2
-        </li>
-        <li className="hidden lg:block dark:text-gray-300 text-sm uppercase font-bold tracking-wide">
-          Court 3
-        </li>
-        {slots.map((slot: string, index: number) => (
-          <>
-            <li className="text-gray-500 mt-4 lg:mt-0 mb-2" key={index}>
-              {slot}
-            </li>
-            <TimeSlotDisplay
-              slot={slot}
-              court="Court 1"
-              courtSlot={`court1slot${index + 1}`}
-              userName={
-                courtSlots &&
-                courtSlots[`court1slot${index + 1}`] === user.userName
-                  ? "cancel"
-                  : user.userName
-              }
-              reservedBy={courtSlots && courtSlots[`court1slot${index + 1}`]}
-              isOpen={courtSlots && courtSlots[`court1slot${index + 1}`] === ""}
-              canCancel={
-                courtSlots &&
-                courtSlots[`court1slot${index + 1}`] === user.userName
-              }
-              reserveSlot={reserveSlot}
-              cancelSlot={cancelSlot}
-            />
-            <TimeSlotDisplay
-              slot={slot}
-              court="Court 2"
-              courtSlot={`court2slot${index + 1}`}
-              userName={
-                courtSlots &&
-                courtSlots[`court2slot${index + 1}`] === user.userName
-                  ? "cancel"
-                  : user.userName
-              }
-              reservedBy={courtSlots && courtSlots[`court2slot${index + 1}`]}
-              isOpen={courtSlots && courtSlots[`court2slot${index + 1}`] === ""}
-              canCancel={
-                courtSlots &&
-                courtSlots[`court2slot${index + 1}`] === user.userName
-              }
-              reserveSlot={reserveSlot}
-              cancelSlot={cancelSlot}
-            />
-            <TimeSlotDisplay
-              slot={slot}
-              court="Court 3"
-              courtSlot={`court3slot${index + 1}`}
-              userName={
-                courtSlots &&
-                courtSlots[`court3slot${index + 1}`] === user.userName
-                  ? "cancel"
-                  : user.userName
-              }
-              reservedBy={courtSlots && courtSlots[`court3slot${index + 1}`]}
-              isOpen={courtSlots && courtSlots[`court3slot${index + 1}`] === ""}
-              canCancel={
-                courtSlots &&
-                courtSlots[`court3slot${index + 1}`] === user.userName
-              }
-              reserveSlot={reserveSlot}
-              cancelSlot={cancelSlot}
-            />
-          </>
-        ))}
-      </ul>
+      
     </>
   );
 };
